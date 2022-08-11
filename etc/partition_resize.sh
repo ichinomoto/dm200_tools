36dfd348628b203b2122a2d08a51262674e658b3
ichinomoto <ichinomoto@gmail.com>
2022-08-12 00:22:46 +0900

    etc: update partition_resize.sh

--- etc/partition_resize.sh
+++ etc/partition_resize.sh
@@ -1,15 +1,16 @@
-#!/bin/sh
+#!/bin/ash
 
-if [ ! "${USER}" = "root" ]; then
-    echo "This script need to do with sudo or root account."
-    exit 1
-fi
+echo "trying to expand rootfs partition"
+/bin/parted -s -a optimal /dev/mmcblk1 -- rm 3                                  # remove swap partition once
+/bin/parted -s -a optimal /dev/mmcblk1 -- mkpart Linux_Swap linux-swap -1080 -1 # make 1GB swap partition at end of SD
+/bin/parted -s -a optimal /dev/mmcblk1 -- resizepart 2 -1081                    # expand rootfs to before swap partition
 
-swapoff /dev/mmcblk1p3
+echo "make swap on /dev/mmcblk1p3"
+/sbin/mkswap /dev/mmcblk1p3
+sync
 
-parted -s -a optimal /dev/mmcblk1 -- rm 3                                  # remove swap partition once
-parted -s -a optimal /dev/mmcblk1 -- mkpart Linux_Swap linux-swap -1000 -1 # make 1GB swap partition at end of SD
-parted -s -a optimal /dev/mmcblk1 -- resizepart 2 -1001                    # expand rootfs to before swap partition
+echo "wait 5sec"
+sleep 5
 
-mkswap /dev/mmcblk1p3
-swapon /dev/mmcblk1p3
+reboot &
+exec /sbin/orig/init
